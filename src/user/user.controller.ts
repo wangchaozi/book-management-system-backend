8ff4b258a9d0167aaf34f4e64e2ecaba689d99bb
王子超 <2758213723@qq.com>
2025-05-27 00:00:34 +0800

    feat：图书管理系统

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register() {
+    console.log();
+    return 'done';
+  }
+}
