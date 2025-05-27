96c0249c821bfa9effc1c6502eb02d4de088089a
王子超 <2758213723@qq.com>
2025-05-27 09:54:12 +0800

    feat: JSON文件存储

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  register() {
-    console.log();
+  register(@Body() registerUserDto: RegisterUserDto) {
+    console.log(registerUserDto);
     return 'done';
   }
 }
